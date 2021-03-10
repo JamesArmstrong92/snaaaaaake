37652161dd4df2bbed315ef188c1a1eb8559d23a
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-10 12:42:41 -0500

    exported connect function

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Data coming from server')
+    console.log(data)
+  })
+
+  conn.on("connect", () => {
+    console.log('Connected!')
+  })
+
+  return conn;
+}
+
+module.exports = { connect };
+
+
