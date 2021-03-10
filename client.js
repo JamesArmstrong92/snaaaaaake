e6f40d7870805dbd6dca1f6383b7c56f2767e2c4
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-10 12:48:50 -0500

    Added name visability

--- client.js
+++ client.js
@@ -9,20 +9,23 @@ const connect = function() {
     host: '135.23.222.131',
     port: 50542
   });
+  conn.on("connect", () => {
+    console.log('Connected!');
+    
+ 
+  });
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
-    console.log('Data coming from server')
-    console.log(data)
-  })
+    console.log('Data coming from server');
+    console.log(data);
+  });
 
-  conn.on("connect", () => {
-    console.log('Connected!')
-  })
+  conn.write('Name: JIM');
 
   return conn;
-}
+};
 
 module.exports = { connect };
 
